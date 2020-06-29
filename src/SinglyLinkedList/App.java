0f15e84da71519e6a2b1da8b050b93c218773119
Labinot Aziri <labi.aziri1@gmail.com>
2020-06-29 17:06:42 +0200

    SLL transformation

--- src/SinglyLinkedList/App.java
+++ src/SinglyLinkedList/App.java
@@ -324,18 +324,47 @@ public class App {
         return result;
     }
 
+
+    public static void listTransformation(SLL<Integer> lista,int X)
+    {
+        /*
+        input:
+        6
+        1 2 5 3 0 7
+        5
+        output:
+        1 2 3 0 5 7
+         */
+
+        SLLNode<Integer> curr = lista.getFirst();
+
+        while (curr != null && curr.element != X) {
+            curr=curr.succ;
+        }
+        System.out.println(curr.element);
+
+        SLLNode<Integer> tmp = curr;
+        while (tmp != null) {
+            if (tmp.element < X) {
+                lista.insertBefore(tmp.element,curr);
+                lista.delete(tmp);
+            }
+            tmp=tmp.succ;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
-        SLL<Character> sll = new SLL<>();
+        SLL<Integer> sll = new SLL<>();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        System.out.println("First List");
         int n = Integer.parseInt(br.readLine());
         String []pomNiza = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
-            sll.insertLast(pomNiza[i].charAt(0));
+            sll.insertLast(Integer.parseInt(pomNiza[i]));
         }
-
+        int x = Integer.parseInt(br.readLine());
+        listTransformation(sll, x);
         System.out.println(sll);
 
         //System.out.println(checkPalindrome(list));
